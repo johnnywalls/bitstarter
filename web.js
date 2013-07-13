3f9a6c4babdee240ffdf2952d82f6ee62604f54b
Juan Paredes <jparedes@mppee.gob.ve>
2013-07-13 13:00:49 -0430

    Read Hello World from index.html

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var file = "index.html";
+  var bf = fs.readFileSync( file );
+  response.send( bf.toString() );
 });
 
 var port = process.env.PORT || 5000;
